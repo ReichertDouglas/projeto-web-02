f9c4b930959b36b7ded1770c6cc7a281bbbbe655
ReichertDouglas <reichert_douglas@hotmail.com>
2025-09-22 20:23:22 -0300

    routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
+  async rewrites() {
+    return [
+      {source: '/', destination:'/home'},
+      {source: '/cadastro', destination:'/auth/signup'},
+      {source: '/login', destination:'/auth/login'},
+      {source: '/dashboard', destination:'/main/dashboard'},
+      {source: '/perfil', destination:'/main/profile'},
+    ]
+  }
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,9 +8,12 @@ export default function Home() {
         <h1 className="font-bold">Noble Finances</h1>
         <div className="flex gap-8">
           <button className="hover:cursor-pointer">Serviços</button>
-          <button className="bg-emerald-800 px-4 py-2 rounded-full text-white hover:cursor-pointer">
+          <Link
+            href="/cadastro"
+            className="bg-emerald-800 px-4 py-2 rounded-full text-white hover:cursor-pointer"
+          >
             Cadastrar-se
-          </button>
+          </Link>
         </div>
       </header>
       <section className="flex w-full">
@@ -19,9 +23,12 @@ export default function Home() {
           <p className="my-8 text-lg">
             Seu sistema de gestão financeira de maneira simples e descomplicada
           </p>
-          <button className="text-white bg-emerald-800 text-lg py-2 px-4 rounded-full hover:cursor-pointer">
+          <Link
+            href="/cadastro"
+            className="text-white bg-emerald-800 text-lg py-2 px-4 rounded-full hover:cursor-pointer"
+          >
             Registre-se e comece a organizar suas finanças
-          </button>
+          </Link>
         </div>
         <div className="w-1/2 flex justify-end p-10">
           <Image src="/globe.png" alt="globe" width={500} height={500} />
