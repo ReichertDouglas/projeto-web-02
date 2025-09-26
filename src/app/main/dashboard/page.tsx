7f0f6f636c6614f2f5d83b040b357cb6e5778fad
ReichertDouglas <reichert_douglas@hotmail.com>
2025-09-25 21:17:44 -0300

    Apis dashboard

--- src/app/main/dashboard/page.tsx
+++ src/app/main/dashboard/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { cotacao, getCripto, getDolar } from "@/app/api";
 import { PizzaGraph } from "@/components/features/dashboard/PizzaGraph";
 import { Header } from "@/components/shared/header";
 import { Navbar } from "@/components/shared/navbar/page";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function DashboardPage() {
   // Mock de dados – substitua depois por chamadas reais
@@ -26,15 +27,55 @@ export default function DashboardPage() {
     { id: 2, nome: "Internet", vencimento: "27/09/2025", valor: 120 },
   ]);
 
-  const [cotacoes] = useState({
-    dolar: "R$ 5,42",
-    ibov: "118.320 pts",
-  });
+  const [dolar, setDolar] = useState<string>("Carregando...");
+  const [cripto, setCripto] = useState<cotacao>();
+  const [btc, setBtc] = useState<number>();
+  const [eth, setEth] = useState<number>();
+
+  useEffect(() => {
+    async function fetchDolar() {
+      try {
+        const valor = await getDolar();
+        if (valor) {
+          setDolar(`R$ ${parseFloat(valor).toFixed(2)}`);
+        } else {
+          setDolar("Cotação indisponível");
+        }
+      } catch (error) {
+        console.error(error);
+        setDolar("Erro ao buscar cotação");
+      }
+    }
+    async function fetchCripto() {
+      try {
+        const valor = await getCripto();
+        console.log(valor)
+        if (valor !== "Cotação não encontrada") {
+          setCripto(valor)
+        } else {
+          console.log("Cotação não encontrada");
+        }
+      } catch (error) {
+        console.error(error);
+        console.log("Erro ao buscar cotação");
+      }
+    }
+
+    fetchCripto();
+    fetchDolar();
+  }, []);
+  
+  useEffect(()=>{
+    setBtc(parseFloat(cripto?.BTCUSD.bid || "0"))
+    setEth(parseFloat(cripto?.ETHUSD.bid || "0"))
+  }, [cripto])
+
+  
 
   return (
     <div className="min-h-screen bg-emerald-300 text-emerald-800 font-serif p-9">
       <div className="max-w-6xl mx-auto">
-        <Header/>
+        <Header />
         <Navbar />
         {/* Balanço Geral */}
         <section className="grid gap-6 md:grid-cols-3 mb-10">
@@ -63,7 +104,7 @@ export default function DashboardPage() {
           <div className="grid md:grid-cols-2 gap-6">
             {/* Placeholder gráfico de pizza */}
             <div className="h-64 flex items-center justify-center text-emerald-800 border rounded">
-              <PizzaGraph data={despesasCategoria}/>
+              <PizzaGraph data={despesasCategoria} />
             </div>
             {/* Tabela resumo */}
             <table className="w-full text-lg">
@@ -115,11 +156,15 @@ export default function DashboardPage() {
           <div className="flex gap-10">
             <div>
               <p className="text-sm text-emerald-800">Dólar (USD)</p>
-              <p className="text-lg font-bold">{cotacoes.dolar}</p>
+              <p className="text-lg font-bold">{dolar.replaceAll(".",",")}</p>
             </div>
             <div>
-              <p className="text-sm text-emerald-800">Ibovespa</p>
-              <p className="text-lg font-bold">{cotacoes.ibov}</p>
+              <p className="text-sm text-emerald-800">Bitcoin</p>
+              <p className="text-lg font-bold">$ {btc?.toLocaleString("pt-br")}</p>
+            </div>
+            <div>
+              <p className="text-sm text-emerald-800">Ethereum</p>
+              <p className="text-lg font-bold">$ {eth?.toLocaleString("pt-br")}</p>
             </div>
           </div>
         </section>
--- src/components/shared/navbar/page.tsx
+++ src/components/shared/navbar/page.tsx
@@ -4,15 +4,15 @@ export const Navbar = () => {
   return (
     <div className="w-full min-h-fit text-xl rounded-xl font-semibold bg-black/20 text-emerald-800 flex p-4 mb-10">
       <div className="flex gap-10 w-full justify-center">
-        <Link href="/home" className="w-2/12 py-1 hover:bg-black/10 rounded-xl text-center">
-          Home
-        </Link>
-        <Link href="/dashboard" className="w-2/12 py-1 hover:bg-black/10 rounded-xl text-center">
+        <Link href="/dashboard" className="w-3/12 py-1 hover:bg-black/10 rounded-xl text-center">
           Dashboard
         </Link>
-        <Link href="/profile" className="w-2/12 py-1 hover:bg-black/10 rounded-xl text-center">Perfil</Link>
+        <Link href="/home" className="w-3/12 py-1 hover:bg-black/10 rounded-xl text-center">
+          Receita/Despesa
+        </Link>
+        <Link href="/profile" className="w-3/12 py-1 hover:bg-black/10 rounded-xl text-center">Perfil</Link>
       </div>
-      <Link href="/logout" className="w-2/12 py-1 bg-emerald-800 rounded-xl text-center text-white">
+      <Link href="/logout" className="w-3/12 py-1 bg-emerald-800 rounded-xl text-center text-white">
         Logout
       </Link>
     </div>
